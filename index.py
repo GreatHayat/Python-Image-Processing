aef2b4234f384070b0eabae0b5d116a2f4bed48b
Khizar Hayat <greathayat@gmail.com>
2019-03-01 15:39:48 +0500

    Update index.py

--- index.py
+++ index.py
@@ -1,28 +1,42 @@
+#from jug import TaskGenerator
 import time
-import multiprocessing as mp
 import cv2 as cv 
 import os 
-def path_function(path, extension):
-        return [os.path.join(path,item) for item in os.listdir(path) if item.endswith(extension)]
-def rotateImages(path):
-        filenames = path("./images", ".jpg")
-        images = []
-        for file in filenames:
-                images.append(cv.imread(file))
-        num = 0
-        for image in images:
-                width, height = image.shape[:2]
-                for angle in range(0,375,15):
-                        rotation = cv.getRotationMatrix2D((width/2, height/2), angle, 0.4)
-                        rotate_images = cv.warpAffine(image, rotation, (width, height))
-                        dir_name = f"rotated_images"
-                        os.makedirs(dir_name, exist_ok=True)
-                        cv.imwrite(os.path.join(dir_name,f"angle_{num}.jpg"), rotate_images)
+import multiprocessing as mp
+def path_function(path):
+    return [file for file in os.listdir(path) if file.endswith(".jpg") & os.path.isfile(os.path.join(path, file))]
 
-                        num += 15
-starttime = time.time()
-pool = mp.Pool(processes=4)
-pool.apply(rotateImages, args=(path_function,))
-print('That took {} seconds'.format(time.time() - starttime))
+def load_images(path):
+    all_files = path(path)
+    return all_files
+def rotate_image(image, name, extension):
+    image = cv.imread(image)
+    width, height = image.shape[:2]
+    num = 0
+    folder = 0
 
+    for angle in range(0,375,15):
+        rotation = cv.getRotationMatrix2D((width/2, height/2), angle, 0.4)
+        rotate_image = cv.warpAffine(image, rotation, (width, height))
+        dir_name = f"angle_{num}"
+        os.makedirs(dir_name, exist_ok=True)
+        cv.imwrite(os.path.join(dir_name,f"{name}_{num}{extension}"), rotate_image)
+        num += 15
+    
+if __name__ == "__main__":
+    starttime = time.time()
+    all_files = path_function(".")
+    processes = []
+    images = []
+    for file in all_files:
+        images.append(file)
+
+    pool = mp.Pool(processes=4)
+    for image in images:
+        name , extension = os.path.splitext(image)
+        pool.apply(rotate_image, args=(image, name, extension,))
+    print('That took {} seconds'.format(time.time() - starttime))
+
+cv.waitKey(0)
+cv.destroyAllWindows()
 
